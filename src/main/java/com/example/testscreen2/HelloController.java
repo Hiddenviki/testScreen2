6edff6980ba64f222ed4d033803aa53476742809
Victorya <Veselkova.v@yandex.ru>
2022-11-01 16:56:00 +0300

    2

--- src/main/java/com/example/testscreen2/HelloController.java
+++ src/main/java/com/example/testscreen2/HelloController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.VBox;
+import org.jetbrains.annotations.NotNull;
 
 import java.net.URL;
 import java.util.HashMap;
@@ -17,15 +18,31 @@ public class HelloController implements Initializable {
 
     @FXML
     private ChoiceBox<String> subject1;
+    @FXML
+    private ChoiceBox<String> subject2;
+    @FXML
+    private ChoiceBox<String> subject3;
 
     @FXML
     private Button moreButton;
 
     @FXML
     private TextField score1;
+    @FXML
+    private TextField score2;
+    @FXML
+    private TextField score3;
 
     @FXML
     private ToolBar toolBar1;
+    @FXML
+    private ToolBar toolBar2;
+    @FXML
+    private ToolBar toolBar3;
+
+    @FXML
+    @NotNull
+    private VBox vBox1;
 
     public Integer count=1;
 
@@ -36,6 +53,11 @@ public class HelloController implements Initializable {
     private String[] availableDirections={};
     //после того как человечек вводит экзамены тут появляются доступные направления
 
+    private String pickedSubject;
+    private Integer score;
+
+
+
     private HashMap<String,Integer> currentExams = new HashMap<>(); //{экзамен:Баллы} //сюда надо
 
     @Override
@@ -44,33 +66,106 @@ public class HelloController implements Initializable {
 //        Enrollee enrollee1 = new Enrollee()
 
 
+
         subject1.getItems().addAll(massiveOfSubjects);
-        subject1.setOnAction(this::addPickedSubjAndScore);
-//        moreButton.setOnAction(event ->
-//        {
-//            if (count<4)
-//            {
-//                ToolBar toolBar = new ToolBar();
+        subject1.setOnMouseClicked(mouseEvent -> {
+            subject1.setOnAction(this::addPickedSubjAndScore);
+        });
+
+
+
+        moreButton.setOnAction(event ->
+        {
+            if (count==1)
+            {
+
+                //vBox1.setPadding(new Insets(10, 10));
+//                vBox1.setSpacing(10);
 //                ChoiceBox<String> subject2 = new ChoiceBox<>();
-//                subject2.getItems().addAll(massiveOfSubjects);
-//                subject2.setOnAction(this::addPickedSubjAndScore);
-//
-//                toolBar(subject1,score1);
-//                count+=1;
-//            }
-//
-//
-//        });
+//                TextField score2 = new TextField();
+
+
+
+                toolBar2 = new ToolBar();
+
+                vBox1.getChildren().add(toolBar2);
+
+                subject2.getItems().addAll(massiveOfSubjects);
+
+                subject2.setOnAction(this::addPickedSubjAndScore);
+
+                toolBar2.getItems().addAll(subject2,score2);
+
+
+                count+=1;
+
+
+            }
+
+            if (count==2)
+            {
+
+                //vBox1.setPadding(new Insets(10, 10));
+//                vBox1.setSpacing(10);
+//                ChoiceBox<String> subject2 = new ChoiceBox<>();
+//                TextField score2 = new TextField();
+                toolBar3 = new ToolBar(subject3,score3);
+
+                vBox1.getChildren().add(toolBar3);
+
+
+                subject3.getItems().addAll(massiveOfSubjects);
+                subject3.setOnAction(this::addPickedSubjAndScore);
+
+                count+=1;
+            }
+
+
+        }); //тут пока что везде хуйня
 
 
     }
 
     public void addPickedSubjAndScore(ActionEvent event)
     {
-        if (score1.getText()!=null && subject1.getValue()!=null)
+
+        if (count==1)
         {
-            String pickedSubject = subject1.getValue();
-            Integer score = Integer.valueOf(score1.getText());
+
+            try {
+
+                toolBar1=new ToolBar(subject1, score1);
+                vBox1=new VBox(toolBar1);
+
+                pickedSubject = subject1.getValue();
+                score = Integer.valueOf(score1.getText());
+                //pickedSubject = subject1.getValue();
+
+                currentExams.put(pickedSubject, Integer.valueOf(score));
+                System.out.println(currentExams);
+            } catch (Exception e) {
+                System.out.println(e.getCause());
+            }
+
+
+        }
+        else if (count==2)
+        {
+            try{
+
+                pickedSubject = subject2.getValue();
+                score = Integer.valueOf(score2.getText());
+                currentExams.put(pickedSubject,score);
+
+                System.out.println(currentExams);
+            }catch (Exception e){System.out.println(e.getCause());}
+            currentExams.put(pickedSubject,score);
+
+        }
+        else if (count==3)
+        {
+            pickedSubject = subject3.getValue();
+            score = Integer.valueOf(score3.getText());
             currentExams.put(pickedSubject,score);
 
             System.out.println(currentExams);
